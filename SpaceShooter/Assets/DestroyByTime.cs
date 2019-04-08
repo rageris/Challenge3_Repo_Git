ab565159e9083ddcf6f428fb8037fb895ac33266
Joel Arroyo <joelarroyo1203@gmail.com>
2019-04-07 21:02:46 -0400

    finished Challenge 3

---
+++ SpaceShooter/Assets/DestroyByTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour {
+
+    public float lifeTime;
+
+	void Start ()
+    {
+        Destroy(gameObject, lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
