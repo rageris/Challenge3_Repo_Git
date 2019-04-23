ab470079a34f7d8efa7da08a184c55670efc0c97
Joel Arroyo <joelarroyo1203@gmail.com>
2019-04-23 14:31:00 -0400

    feature complete

---
+++ SpaceShooter/Assets/PickUp.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    public GameObject player;
+
+    private float timer;
+    private bool isBoosted;
+    private PlayerController playerController;
+    private Color tmp;
+
+    void Start()
+    {
+        playerController = player.GetComponent<PlayerController>();
+
+        tmp = GetComponent<SpriteRenderer>().color;
+
+        isBoosted = false;
+
+        timer = 10f;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isBoosted = true;
+
+            tmp.a = 0f;
+
+            GetComponent<SpriteRenderer>().color = tmp;
+
+            GetComponent<SphereCollider>().enabled = false;
+        }
+
+    }
+
+    void Update ()
+    {
+        if (isBoosted)
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+
+                playerController.speed = 15f;
+            }
+                else 
+            {
+                playerController.speed = 10f;
+
+                isBoosted = false;
+            }
+    }
+}
---
+++ SpaceShooter/Assets/PickUpMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpMover : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.velocity = transform.up  * speed;
+    }
+}
